2be679390ba15be56e3f4973e89957b8fae1da4f
Joel Hassan <joel.nhn@gmail.com>
2024-07-19 01:14:10 +0300

    fix: safelist dynamic classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+let safelist = [];
+for (let i = 0; i < 604; i++) {
+  safelist.push(`font-[page${i}]`);
+}
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-
+  safelist,
   theme: {
     extend: {},
   },
