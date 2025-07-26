cc4f09ac76fca93c5070488d0704d2d3a8379991
Joel Hassan <joel.nhn@gmail.com>
2025-07-27 00:17:26 +0300

    fix: cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,26 +1,25 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('/*', cors({
-  origin: [process.env.WEB_URL as string],
-}))
+const corsOrigin = process.env.WEB_URL ? [process.env.WEB_URL] : ["*"];
 
-app.get('/api', (c) => {
+app.use("/*", cors({ origin: corsOrigin }));
+app.get("/api", (c) => {
   return c.json({
-    message: 'Hello Hono!'
-  })
-})
+    message: "Hello Hono!",
+  });
+});
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3001
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
-if (process.env.NODE_ENV === 'development') {
-  console.log(`Server is running on http://localhost:${port}/api`)
+if (process.env.NODE_ENV === "development") {
+  console.log(`Server is running on http://localhost:${port}/api`);
 }
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
