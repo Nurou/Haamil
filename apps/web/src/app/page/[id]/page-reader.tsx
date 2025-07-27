a7b7eb1efa99ed8ba81314b6b3951cc115ce0ff4
Joel Hassan <joel.nhn@gmail.com>
2025-07-27 15:33:11 +0300

    chore: try calling api from client

--- apps/web/src/app/page/[id]/page-reader.tsx
+++ apps/web/src/app/page/[id]/page-reader.tsx
@@ -2,30 +2,43 @@ import { Menu } from "@/web/components/menu";
 import { queryClient } from "@/web/queries";
 import { QueryClientProvider } from "@tanstack/react-query";
 import {
-	PageReaderContextProvider,
-	type PageReaderContextType,
+  PageReaderContextProvider,
+  type PageReaderContextType,
 } from "../../../hooks/use-page-reader-context";
 import { PageReaderLines } from "./page-lines";
+import { useEffect, useState } from "react";
 
 function PageReaderLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="flex flex-col h-screen">
-			<main className="flex-grow flex items-center justify-center">
-				{children}
-			</main>
-			<Menu />
-		</div>
-	);
+  console.log("PageReaderLayout");
+  /* make a request to /api  */
+  const [data, setData] = useState<any>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api");
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="flex flex-col h-screen">
+      <main className="flex-grow flex items-center justify-center">
+        {children}
+      </main>
+      <Menu />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default function PageReader(pageRenderData: PageReaderContextType) {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<PageReaderContextProvider value={pageRenderData}>
-				<PageReaderLayout>
-					<PageReaderLines />
-				</PageReaderLayout>
-			</PageReaderContextProvider>
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PageReaderContextProvider value={pageRenderData}>
+        <PageReaderLayout>
+          <PageReaderLines />
+        </PageReaderLayout>
+      </PageReaderContextProvider>
+    </QueryClientProvider>
+  );
 }
