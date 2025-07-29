528ec0e8a0ce5424d6a2b2d72c699275d5710a2b
Joel Hassan <joel.nhn@gmail.com>
2025-07-30 00:53:48 +0300

    fix: add missing build command for api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,7 +5,21 @@ import app from "./app";
 const port = Number(process.env.PORT) || 3001;
 console.log(`Server is running on port http://localhost:${port}`);
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port,
+});
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 });
